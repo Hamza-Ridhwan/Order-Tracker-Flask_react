17f0c30775595e63d8b6ff71e8e5f84217998bc5
hamza-ridhwan <hamzathehamzah@gmail.com>
2025-02-03 01:53:05 +0300

    Added changes

--- server/config.py
+++ server/config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hgjhytgfhg jgjhghgjhg'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///order_tracker.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://ordertrackerdb_user:6NaWmOFmjjpvjSVsYjR5HPNI8S5prkKX@dpg-cufvabq3esus73e5dgq0-a.oregon-postgres.render.com/ordertrackerdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'hgnf hgkjhgjh hghgjh'
